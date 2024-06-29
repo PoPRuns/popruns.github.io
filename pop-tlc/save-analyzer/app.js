b7848c4178e0840877780227cc96f1ef87b6eddd
Pranav G <pranavbtech@gmail.com>
2024-06-29 14:04:54 +0530

    amulets, description system

--- pop-tlc/save-analyzer/app.js
+++ pop-tlc/save-analyzer/app.js
@@ -6,43 +6,53 @@ var renderData = [
         data: {
             MiniMap: {
                 dispalyName: "Eye of the Wanderer",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Map: Given by Fariba"
             },
             BowAim: {
                 dispalyName: "Bow of Menolias",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Found in Hyrcanian Forest"
             },
             ChakramAim: {
                 dispalyName: "Chakram of Menolias",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Find Menolias in the Forest"
             },
             AirDash: {
                 dispalyName: "Rush of the Simurgh",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Dash: Defeat Jahandar"
             },
             Teleport: {
                 dispalyName: "Shadow of the Simurgh",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Found in the Depths"
             },
             Clairvoyance: {
                 dispalyName: "Clairvoyance",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Found in Sacred Archives"
             },
             Warp: {
                 dispalyName: "Dimensional Claw",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Found in Soma Tree"
             },
             DoubleJump: {
                 dispalyName: "Gravity Wings",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Double Jump: Defeat Azhdaha"
             },
             ChakramTeleport: {
                 dispalyName: "Chakram Shadow of the Simurgh",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Defeat Menolias"
             },
             ScarfAim: {
                 dispalyName: "Fabric of Time",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Grapple: Defeat Varham"
             }
         }
     },
@@ -53,47 +63,345 @@ var renderData = [
         data: {
             FocusAbilityPiercingAttack: {
                 dispalyName: "Verethragna's Smite",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Obtained from story progression"
             },
             FocusAbilityAntiAir: {
                 dispalyName: "Shahbaz' Spirit",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Alternate Sargon fight in Sacred Archives"
             },
             FocusAbilityWindSlashAttack: {
                 dispalyName: "Wind of Sistan",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Found in the Depths"
             },
             FocusAbilitySuperChargeAttack: {
                 dispalyName: "Vayu's Wave",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Complete the first 9 Artaban's challenges"
             },
             FocusAbilityHeal: {
                 dispalyName: "Bahman's Breath",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "First Wak-Wak tree in Hyrcanian Forest"
             },
             FocusAbilitySuperShotLaserAim: {
                 dispalyName: "Arash's Ray",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Alternate Sargon fight in Depths"
             },
             FocusAbilityGroundSlam: {
                 dispalyName: "Hadhayans' Might",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Alternate Sargon fight in Soma Tree"
             },
             FocusAbilitySuperWarrior: {
                 dispalyName: "Soul of Gilgamesh",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Alternate Sargon fight in Pit of Eternal Sands"
             },
             FocusAbilityMaelstrom: {
                 dispalyName: "Bahamut's Rage",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Orod fight in Raging Sea"
             },
             FocusAbilityFullCounterStance: {
                 dispalyName: "Rashnu's Judgment",
-                isUnlocked: false
+                isUnlocked: false,
+                description: "Menolias fight in Upper City"
+            }
+        }
+    },
+    {
+        title: "Amulets",
+        percentage: 5,
+        description: "Each amulet as well as each amulet upgrade gives 0.051% game completion.\n*Amulets unlocked from the Divine Trials don't give %.",
+        data: {
+            ExtendedHits: {
+                dispalyName: "Blade Dancer",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Obtained from story progression"
+            },
+            DodgeMaster: {
+                dispalyName: "Elusive Water",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Soma Tree"
+            },
+            VoidStone: {
+                dispalyName: "Void Blade",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Upper City"
+            },
+            Finisher: {
+                dispalyName: "Verethrangna's Wrath",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Sunken Harbor"
+            },
+            CounterAttackStone: {
+                dispalyName: "Turning Wind",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Soma Tree, talk to the wolf after defeating Kiana"
+            },
+            DaredevilStone: {
+                dispalyName: "Indomitable Spirit",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "The Depths, complete the “Motherly Love” quest"
+            },
+            Dauntless: {
+                dispalyName: "Arslân's Glory",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, start the “Prophecy of Mount Qaf” quest"
+            },
+            BraveStone: {
+                dispalyName: "Will of Rostam",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Sacred Archives"
+            },
+            ExplosiveStone: {
+                dispalyName: "Agony Amulet",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Defeat the Giant Crab in the Depths"
+            },
+            EvilEyeStone: {
+                dispalyName: "Evil-Eye Amulet",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 1,
+                description: "The Depths, the Scrapper's Shop"
+            },
+            BowMultiShot: {
+                dispalyName: "White Peacock",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Sunken Harbor"
+            },
+            BowFireArrow: {
+                dispalyName: "Blazing Kestrel",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Tower of Silence"
+            },
+            HunterStone: {
+                dispalyName: "Zurvan's Voice",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, Architect puzzle chest"
+            },
+            HawkStone: {
+                dispalyName: "Arash's Arrowhead",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, Kaheva's Forge"
+            },
+            ChakramVortex: {
+                dispalyName: "Chakram Tempest",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, Kaheva's Forge"
+            },
+            LifeStone: {
+                dispalyName: "Blessing",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, The Mage"
+            },
+            Prayer: {
+                dispalyName: "Wolf-Bride",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Hyrcanian Forest, defeat Erlik"
+            },
+            PhoenixStone: {
+                dispalyName: "Dragon King",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Sacred Archives, start the “Moon Gatherer” quest"
+            },
+            Monk: {
+                dispalyName: "Starving Heart",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 1,
+                description: "The Depths, the Scrapper's Shop"
+            },
+            SafetyStone: {
+                dispalyName: "Hardiness",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, The Mage"
+            },
+            SteelStone: {
+                dispalyName: "Mount Damavand",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, Kaheva's Forge"
+            },
+            ShieldOfMithra: {
+                dispalyName: "Shield of Mithra",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, complete the “Ancient Power Unearthed” quest"
+            },
+            Knight: {
+                dispalyName: "Gleaming Lion",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Lower City"
+            },
+            Tower: {
+                dispalyName: "Rukhsana's Gift",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Lower City"
+            },
+            TeleportShockWave: {
+                dispalyName: "Shockwave",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "The Depths, Architect puzzle chest"
+            },
+            TeleportRay: {
+                dispalyName: "Divine Spear",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in The Depths"
+            },
+            DarkEye: {
+                dispalyName: "Eye of Destiny",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 1,
+                description: "The Depths, the Scrapper's Shop"
+            },
+            WealthStone: {
+                dispalyName: "Ard's Fortune",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Sunken Harbor, Architect puzzle chest"
+            },
+            MagnetStone: {
+                dispalyName: "Ecbatana Seal",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 1,
+                description: "Lower City, Kaheva's Forge"
+            },
+            CelestialStone: {
+                dispalyName: "Four Royal Stars",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Upper City"
+            },
+            Treasure: {
+                dispalyName: "King Jamshid",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 1,
+                description: "Lower City, The Mage"
+            },
+            Focus: {
+                dispalyName: "Ayyar Amulet",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in The Depths"
+            },
+            Poison: {
+                dispalyName: "Horned Viper",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, The Mage"
+            },
+            Flame: {
+                dispalyName: "Holy Fire",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Lower City, progress in the “Prophecy of Mount Qaf” quest"
+            },
+            Thunder: {
+                dispalyName: "Thunder Charm",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Sunken Harbor"
+            },
+            Ice: {
+                dispalyName: "Frost Charm",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 3,
+                description: "Chest in Lower City"
+            },
+            FireStormCharge: {
+                dispalyName: "Flaming Whirlwind*",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 1,
+                description: "Complete the Peacock memorial"
+            },
+            SuperArrow: {
+                dispalyName: "Point of Honor*",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 1,
+                description: "Complete the Stallion memorial"
+            },
+            RefillArrow: {
+                dispalyName: "Arash's Quiver*",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 1,
+                description: "Complete the Hound memorial"
+            },
+            FrozenShield: {
+                dispalyName: "Frozen Truth*",
+                isUnlocked: false,
+                level: 0,
+                maxLevel: 1,
+                description: "Complete the Arouch memorial"
             }
         }
     }
 ];
+const initialData = structuredClone(renderData);
 
 function renderDataToHTML() {
     const mainDiv = document.getElementById('dynamic');
@@ -119,9 +427,22 @@ function renderDataToHTML() {
         sectionData.classList.add('section-data');
         for (const key in section.data) {
             const value = section.data[key];
-            const listItem = document.createElement('p');
+            const listItemDiv = document.createElement('div');
+            listItemDiv.classList.add("section-list-item");
+            const listItem = document.createElement('span');
             listItem.innerText = `${value.isUnlocked ? "☑️" : "❌"} ${value.dispalyName}`;
-            sectionData.appendChild(listItem);
+            listItemDiv.appendChild(listItem);
+            if (value.level > 0) {
+                const upgradesLeft = value.maxLevel - value.level;
+                value.description = `${value.level === 1 ? "Base" : `+${value.level - 1}`} Unlocked${upgradesLeft ? `, ${upgradesLeft} Upgrades Left` : ""}`;
+            }
+            if (value.description) {
+                const itemDesc = document.createElement('span');
+                itemDesc.classList.add('item-description');
+                itemDesc.innerText = ` — ${value.description}`;
+                listItemDiv.appendChild(itemDesc);
+            }
+            sectionData.appendChild(listItemDiv);
         }
         mainDiv.appendChild(sectionData);
     });
@@ -136,7 +457,8 @@ function processJson(jsonObj) {
     const entityManager = jsonObj.m_list.find(obj => obj.m_sceneName === "EntitiesManager");
     const progressionData = entityManager.m_entities[0].m_frames[0].m_dataList.find(obj => obj && obj["@type"] === "Alkawa.Gameplay.PlayerProgressionSubComponent+PlayerProgressionSaveData");
     const unlockedAbilites = progressionData.m_progression.m_playerAbilitiesProgressionData.m_unlockedAbilities;
-    console.log(unlockedAbilites);
+    const unlockedAmulets = progressionData.m_progression.m_playerStoneOfKnowledgeProgressionData.m_unlockedStoneOfKnowledges;
+    const amuletLevels = progressionData.m_progression.m_playerStoneOfKnowledgeProgressionData.m_stoneLevels;
     var abilitySectionData = renderData.find(section => section.title === "Abilities").data;
     for (var ability in abilitySectionData) {
         abilityProps = abilitySectionData[ability];
@@ -157,9 +479,21 @@ function processJson(jsonObj) {
             surgeProps.isUnlocked = false;
         }
     }
+    var amuletSectionData = renderData.find(section => section.title === "Amulets").data;
+    for (var amulet in amuletSectionData) {
+        amuletProps = amuletSectionData[amulet];
+        if (unlockedAmulets.includes(amulet)) {
+            amuletProps.isUnlocked = true;
+            const amuletLevel = amuletLevels[unlockedAmulets.indexOf(amulet)];
+            amuletProps.level = amuletLevel;
+        }
+        else {
+            amuletProps.isUnlocked = false;
+            amuletProps.level = 0;
+            amuletProps.description = initialData.find(section => section.title === "Amulets").data[amulet].description;
+        }
+    }
     renderDataToHTML();
-
-    console.log("Unlocked Abilites:", unlockedAbilites);
 }
 
 function handleChangeSaveFile(e) {
